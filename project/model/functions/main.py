71fb8ca7b0fe4bc896d28ffcd398e9346dd606cc
vincetyy <97782480+vincetyy@users.noreply.github.com>
2024-07-06 00:51:19 +0800

    removing unneeded files

--- project/model/functions/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Add the src directory to the sys.path so we can import main from there
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
-
-from src.main import handler
-
-# Now Netlify can use the handler to process requests
